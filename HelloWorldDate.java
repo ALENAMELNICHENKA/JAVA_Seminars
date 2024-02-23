ecf87004b3fa4468ea521c3eb80ffd9fc400ecbf
Alena Melnichenka <6983314@mail.ru>
2024-02-23 13:04:56 +0300

    Файл с вводными данными по java

---
+++ HelloWorldDate.java
@@ -0,0 +1,40 @@
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+/**
+ * HelloWorldDate
+ */
+public class HelloWorldDate {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World");
+
+        System.out.println(LocalDateTime.now());
+
+        /* 
+        Типы данных:
+
+        int num = 3;
+        double d = 4.6;
+        float d = 4.6; но меньше знаков после запятой
+        String = "Hello";
+        char c = 't'; - символьный тип данных
+        boolen b = true;
+
+       Объявление массивов:
+        
+        int[] array = new int[6];
+        int[] array1 = {5, 5, 8, 3, 2, 1}; если сразу задаём значение
+
+        for (int i = 0; i < array1.length; i++)
+            System.out.println(array[i]);
+
+            Вывод данных - System.out.println(array[i]);
+            Ввод данных - Scanner scanner = new Scanner(System.in); - открываем внешний поток(из терминала берутся данные)
+            scanner.close(); - закрыть поток
+       */ 
+      
+    }
+    
+}
